a1d1c79ae316a8870f77e4b03c3d9c2e70c882e6
Josie Smith <jsmit194@syr.edu>
2025-01-27 18:15:12 -0500

    Challenge 4 complete

--- Code Challenges/Challenge 1-1-4.py
+++ Code Challenges/Challenge 1-1-4.py
@@ -1,11 +1,11 @@
 numbergrade = int(input('Enter your number grade: '))
-if numbergrade >= 95:
+if numbergrade >= 95 and numbergrade <= 120:
     lettergrade = 'A'
 elif numbergrade >= 75 and numbergrade < 95:
     lettergrade = 'B'
 elif numbergrade >= 50 and numbergrade < 75:
     lettergrade = 'C'
-elif numbergrade < 50:
+elif numbergrade < 50 and numbergrade < 0:
     lettergrade = 'F'
 elif numbergrade > 120 or numbergrade < 0:
     lettergrade = 'Bad grade'
